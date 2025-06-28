a989f09a053165745f6648e385b5cfa2308913e4
Dazbo <darren_lester@epam.com>
2025-06-28 23:10:56 +0100

    secret prompts and logger refactor

--- src/rickbot/config.py
+++ src/rickbot/config.py
@@ -5,26 +5,13 @@ from pathlib import Path
 from dataclasses import dataclass
 import streamlit as st
 
+APP_NAME = "Rickbot"
 SCRIPT_DIR = Path(__file__).parent
 
-@dataclass
-class Config:
-    """Holds application configuration."""
-    project_id: str
-    region: str
-    logger: logging.Logger
-    auth_required: bool # Whether we require logon
-    rate_limit: int # How many model requests we can make
-
-@st.cache_resource
-def get_config(app_name: str) -> Config:
-    """
-    Retrieves environment variables, initializes the logger, and returns a configuration object.
-    This function is cached and runs only once per session.
-    """
-    # --- Logger Initialization ---
+def setup_logger() -> logging.Logger:
+    """Sets up and configures a logger for the application."""
     log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
-    app_logger = logging.getLogger(app_name)
+    app_logger = logging.getLogger(APP_NAME)
     log_level_num = getattr(logging, log_level, logging.INFO)
     app_logger.setLevel(log_level_num)
     
@@ -41,6 +28,28 @@ def get_config(app_name: str) -> Config:
     app_logger.info("Logger initialised.")
     app_logger.debug("DEBUG level logging enabled.")    
 
+    return app_logger
+
+logger = setup_logger()
+
+@dataclass
+class Config:
+    """Holds application configuration."""
+    project_id: str
+    region: str
+    auth_required: bool # Whether we require logon
+    rate_limit: int # How many model requests we can make
+
+# @st.cache_resource
+# def get_google_project():
+#     return os.environ.get('GOOGLE_CLOUD_PROJECT')
+
+@st.cache_resource
+def get_config() -> Config:
+    """
+    Retrieves environment variables, initializes the logger, and returns a configuration object.
+    This function is cached and runs only once per session.
+    """
     # --- Environment Variable Retrieval and Validation ---
     project_id = os.environ.get('GOOGLE_CLOUD_PROJECT')
     region = os.environ.get('GOOGLE_CLOUD_REGION')
@@ -48,22 +57,21 @@ def get_config(app_name: str) -> Config:
     limit = int(os.environ.get("RATE_LIMIT", "20"))
     
     if not project_id:
-        app_logger.error("Configuration Error: GOOGLE_CLOUD_PROJECT not set.")
+        logger.error("Configuration Error: GOOGLE_CLOUD_PROJECT not set.")
         st.error("🚨 Configuration Error: Cannot determine Project ID.")
         st.stop()
 
     if not region:
-        app_logger.warning("Could not determine Google Cloud Region. Using 'global'.")
+        logger.warning("Could not determine Google Cloud Region. Using 'global'.")
         st.warning("⚠️ Could not determine Google Cloud Region. Using 'global'.")
         region = "global"
 
-    app_logger.info(f"Using Google Cloud Project: {project_id}")
-    app_logger.info(f"Using Google Cloud Region: {region}")
-    app_logger.info(f"Auth required: {auth_required}")
-    app_logger.info(f"Rate limit: {limit}")
+    logger.info(f"Using Google Cloud Project: {project_id}")
+    logger.info(f"Using Google Cloud Region: {region}")
+    logger.info(f"Auth required: {auth_required}")
+    logger.info(f"Rate limit: {limit}")
 
     return Config(project_id=project_id, 
                   region=region, 
-                  logger=app_logger, 
                   auth_required=auth_required,
                   rate_limit=limit)
--- src/rickbot/create_auth_secrets.py
+++ src/rickbot/create_auth_secrets.py
@@ -1,9 +1,10 @@
 """ Creates local .streamlit/secrets.toml from secret in Google Secret Manager 
 We can run this from our app code or standalone. """
 import os
-from functools import lru_cache
-from google.cloud import secretmanager
 import streamlit as st
+
+from utils import retrieve_secret
+from config import logger
 
 @st.cache_resource
 def create_secrets_toml(google_project_id: str):
@@ -11,29 +12,23 @@ def create_secrets_toml(google_project_id: str):
     secrets_file_path = os.path.join(streamlit_dir, "secrets.toml")
     
     if os.path.exists(secrets_file_path):
-        print(".streamlit/secrets.toml already exists, skipping creation.")
+        logger.info(".streamlit/secrets.toml already exists, skipping creation.")
         return # Nothing to do
 
-    print("Retrieving OAuth credentials.")
-    secret_name = os.environ.get("STREAMLIT_SECRETS_SECRET_NAME", "rickbot-streamlit-secrets-toml")
-    secret_version = os.environ.get("STREAMLIT_SECRETS_SECRET_VERSION", "latest")
-
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{google_project_id}/secrets/{secret_name}/versions/{secret_version}"
-
+    logger.info("Retrieving OAuth credentials.")
     try:
-        response = client.access_secret_version(request={"name": name})
-        secrets_content = response.payload.data.decode("UTF-8")
+        secret_name = "rickbot-streamlit-secrets-toml"
+        secret = retrieve_secret(google_project_id, secret_name)
 
         os.makedirs(streamlit_dir, exist_ok=True)
-        print(".streamlit/ created.")
+        logger.info(".streamlit/ created.")
         with open(secrets_file_path, "w") as f:
-            f.write(secrets_content)
-        print(f"Successfully created {secrets_file_path}")
+            f.write(secret)
+        logger.info(f"Successfully created {secrets_file_path}")
 
     except Exception as e:
         raise ValueError(f"Error accessing secret '{secret_name}' from Secret Manager: {e}") from e
 
 if __name__ == "__main__":
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-    create_secrets_toml(project_id)
+    create_secrets_toml(project_id) # type: ignore
